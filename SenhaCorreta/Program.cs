6580234f016d85fefc3c37c64c9b742b88310f2e
Lucas Levi <138832200+LucasL-Dev@users.noreply.github.com>
2025-07-10 23:12:28 -0300

    Initial commit

---
+++ SenhaCorreta/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SenhaCorreta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int SenhaC = 2811;
+            Console.Write("Digite uma senha: ");
+            int Senha = int.Parse(Console.ReadLine());
+            while (Senha != SenhaC)
+            {
+                Console.WriteLine("SENHA INCORRETA");
+                Console.Write("Tente novamente: ");
+                Senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha Correta!");
+        }
+    }
+}
